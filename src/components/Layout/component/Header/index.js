5ad4d38e3fbbf5a983afae08ad6f53873973259b
yangdi0710 <phanlengocduy07102000@gmail.com>
2024-01-10 17:13:30 +0700

    10/1

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -18,9 +18,12 @@ function Header() {
                     <button className={cx('clear')}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
-                    {/* {Loading} */}
-                    <button className={cx('search-btn')}></button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
