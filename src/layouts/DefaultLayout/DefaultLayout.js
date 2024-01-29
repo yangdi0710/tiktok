f8d0a5cff3bd8341af635ea2c70f6decd04537f4
yangdi0710 <phanlengocduy07102000@gmail.com>
2024-01-29 17:48:21 +0700

    29/1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,5 +1,5 @@
 import Header from '~/layouts/component/Header';
-import Sidebar from './Sidebar';
+import Sidebar from '~/layouts/component/Sidebar';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 import PropTypes from 'prop-types';
