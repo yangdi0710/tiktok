07536823de4723199ad989ab672b95f417ae31a9
yangdi0710 <phanlengocduy07102000@gmail.com>
2024-01-30 16:41:03 +0700

    30/1

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,44 @@
+import classNames from 'classnames/bind';
+import styles from './SuggestedAccounts.module.scss';
+
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    const renderPreview = (props) => {
+        return (
+            <div className={cx('preview')} tabIndex="-1" {...props}>
+                <PopperWrapper>Item review</PopperWrapper>
+            </div>
+        );
+    };
+    return (
+        <div>
+            <Tippy interactive delay={[700, 0]} render={renderPreview} placement="bottom">
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbad5d98186083a8308542a43904d4ac.jpeg?lk3s=a5d48078&x-expires=1706770800&x-signature=P1V%2BlT4%2BY%2BptnC0Axx4ij9%2FHhTw%3D"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>anguyen</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Nguyen Van A</p>
+                    </div>
+                </div>
+            </Tippy>
+        </div>
+    );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
--- src/layouts/component/Sidebar/Sidebar.js
+++ src/layouts/component/Sidebar/Sidebar.js
@@ -10,6 +10,7 @@ import {
     LiveActiveIcon,
 } from '~/components/Icons';
 import Menu, { MenuItem } from './Menu';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
 
 const cx = classNames.bind(styles);
 
@@ -26,6 +27,8 @@ function Sidebar() {
                 />
                 <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
+            <SuggestedAccounts label="Suggested Accounts" />
+            <SuggestedAccounts label="Following Accounts" />
         </aside>
     );
 }
